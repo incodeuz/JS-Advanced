9695b4a46da180e1eb9a0378f0f2f7b5139579af
Abdulloh Solih <qiyomovabdulloh3@gmail.com>
2021-12-01 15:19:00 +0500

    firstparttaskisdone

--- dars5/dars5.js
+++ dars5/dars5.js
@@ -1,18 +1,16 @@
 const createBtn = document
-.querySelector(".create")
-.addEventListener("click", getStyle);
+  .querySelector(".create")
+  .addEventListener("click", getStyle);
 function getStyle() {
   const inputStyle = document.querySelector(".input-style").value;
-  let box = document.querySelector(".box");
-  let inputBg = document.querySelector(".input-bg").value;
-  box.style.background = inputBg;
-
-  if (inputStyle === 'round') {
-    box.style.borderRadius = `50%`
-  }
-
   let inputWidth = document.querySelector(".input-width").value;
-  box.style.width = inputWidth;
+  let inputBg = document.querySelector(".input-bg").value;
   let inputHeight = document.querySelector(".input-height").value;
+  let box = document.querySelector(".box");
+  box.style.background = inputBg;
+  box.style.width = inputWidth;
   box.style.height = inputHeight;
+  if (inputStyle === "round") {
+    box.style.borderRadius = `50%`;
+  }
 }
