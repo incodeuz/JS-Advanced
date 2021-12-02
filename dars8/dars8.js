6bcfcff4ebd8a73f027913047c3e1c2fafefae13
Abdulloh Solih <qiyomovabdulloh3@gmail.com>
2021-12-02 20:56:31 +0500

    done

--- dars8/dars8.js
+++ dars8/dars8.js
@@ -1,6 +1,9 @@
-const getOverall = document.querySelector('.btn').addEventListener('click', getAge)
+const getOverall = document
+  .querySelector(".btn")
+  .addEventListener("click", getAge);
 function getAge() {
-    let getYear = new Date().getFullYear()
-    let value = document.querySelector('.input').value
-    document.querySelector('.textcha').innerHTML='Sizning yoshingiz hozirda ' + (getYear - value)+ ' ' + 'yoshda'
+  let getYear = new Date().getFullYear();
+  let value = document.querySelector(".input").value;
+  document.querySelector(".textcha").innerHTML =
+    "Sizning yoshingiz hozirda " + (getYear - value) + " " + "yoshda";
 }
